720861f31c1d5c94986713282d60fccccd36407c
sibekov <vusimuzij.sibeko@gmail.com>
2024-12-07 10:21:48 +0000

    Add html js css files bootstrap and basic boiler plates

---
+++ myscript.js
@@ -0,0 +1,4 @@
+
+var introduction =prompt("want to know if we are connected?")
+var myfeedback = alert("your said: "+introduction+ "Thefore: you are connected!!!!!")
+console.log("your said: "+introduction+ "Thefore: you are connected!!!!!")
